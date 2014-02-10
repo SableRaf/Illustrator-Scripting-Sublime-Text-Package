fc8e8989e206aca820a5dfa2a7cb98576e44859b
Raphael de Courville <raphael@acantharia.local>
2014-02-10 17:15:15 +0100

    escaped space characters from build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ base_path="Applications";
 
 # Full path (assumes that your installation of Illustrator is 
 # in your user's Applications folder and not in the shared one)
-full_path="$HOME/${base_path}/Adobe Illustrator ${version}/Presets/${language}/Scripts";
+full_path="$HOME/${base_path}/Adobe\ Illustrator\ ${version}/Presets/${language}/Scripts";
 
 # Copying built file to script folder with some very crude error handling
 if cp $file_name "${full_path}/${file_name%.*}.jsx"; then 
